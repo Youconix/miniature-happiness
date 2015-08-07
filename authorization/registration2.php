ded8a0f40ce8363d251b9b4609d83a4d377b02f6
Roxanna Lugtigheid <rlugtigheid@youconix.nl>
2015-08-07 13:25:52 +0200

    Adding files for 2nd stage registration screen.
    Currently it simply proceeds to actual login, for the time being. It may be used if a webmaster thinks it preferable to request additional information from the user at the time of registration.

---
+++ authorization/registration2.php
@@ -0,0 +1,52 @@
+<?php
+namespace authorization;
+
+use core\BaseLogicClass;
+
+class Registration2 extends \includes\BaseLogicClass
+{
+
+    /**
+     *
+     * @var \core\models\Login
+     */
+    private $login;
+
+    /**
+     *
+     * @var \core\models\User
+     */
+    private $user;
+
+    private $i_userid;
+
+    /**
+     * Constructor
+     *
+     * @param \Input $input
+     *            The input parser
+     * @param \Config $config            
+     * @param \Language $language            
+     * @param \Output $template            
+     * @param \Header $header            
+     * @param \Menu $menu            
+     * @param \Footer $footer            
+     * @param \core\models\Login $login            
+     * @param \core\models\User $user            
+     * @param \core\services\Session $session            
+     */
+    public function __construct(\Input $input, \Config $config, \Language $language, \Output $template, \Header $header, \Menu $menu, \Footer $footer, \core\models\Login $login, \core\models\User $user, \core\services\Session $session)
+    {
+        parent::__construct($input, $config, $language, $template, $header, $menu, $footer);
+        
+        $this->login = $login;
+        $this->user = $user;
+        $this->i_userid = $this->session->get('userid');
+    }
+
+    protected function index()
+    {
+        $user = $this->user->get($this->i_userid);
+        $this->login->setLogin($user);
+    }
+}
